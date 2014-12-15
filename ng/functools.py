7b47ff1682d5a70952e879477f21c229bed8cfc4
NerdWizard <eizoassik@gmail.com>
2014-12-15 19:58:45 +0800

    better partical

--- ng/functools.py
+++ ng/functools.py
@@ -1,11 +1,13 @@
 # encoding=utf8
 from functools import reduce as _reduce
+from functools import partial as _partial
 
 
 class composition:
     """
     works like DOT (.) in Haskell
     """
+
     def __init__(self, *args):
         if len(args) < 1:
             raise ValueError(
@@ -19,6 +21,37 @@ class composition:
         for fn in self._functions:
             v = fn(v)
         return v
+
+
+class partial:
+    def __init__(self, func, *args, position=None, **kwargs):
+        if position and args:
+            raise ValueError("Cannot use specified position arguments"
+                             + " with non-specified position arguments"
+                             + " at same time.")
+        if position:
+            self._position = position
+            self._func = func
+        else:
+            self._position = False
+            self._func = _partial(func, *args, **kwargs)
+        self._kwargs = kwargs
+
+    def __call__(self, *args, **kwargs):
+        if self._position:
+            iargs = iter(args)
+            mixed_args = []
+            for i in range(len(args) + len(self._position)):
+                if i in self._position:
+                    mixed_args.append(self._position[i])
+                else:
+                    mixed_args.append(next(iargs))
+            mixed_kwargs = dict(self._kwargs)
+            mixed_kwargs.update(kwargs)
+            return self._func(*mixed_args, **mixed_kwargs)
+        else:
+            return self._func(*args, **kwargs)
+
 
 
 fold = _reduce
